584b78822f280ef314c652e9527a104ef91bf3e1
Alex_V <venedykhin@yandex.ru>
2025-07-28 19:14:34 +1200

    Обновлен  routes/customers.ts

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -7,13 +7,12 @@ import {
 } from '../controllers/customers'
 import auth from '../middlewares/auth'
 import { adminGuard } from '../middlewares/adminGuard'
-import { apiRateLimiter } from '../middlewares/rateLimiter'
 
 const customerRouter = Router()
 
-customerRouter.get('/', apiRateLimiter, auth, adminGuard, getCustomers)
-customerRouter.get('/:id', apiRateLimiter, auth, adminGuard, getCustomerById)
-customerRouter.patch('/:id', apiRateLimiter, auth, adminGuard, updateCustomer)
-customerRouter.delete('/:id', apiRateLimiter, auth, adminGuard, deleteCustomer)
+customerRouter.get('/', auth, adminGuard, getCustomers)
+customerRouter.get('/:id', auth, adminGuard, getCustomerById)
+customerRouter.patch('/:id', auth, adminGuard, updateCustomer)
+customerRouter.delete('/:id', auth, adminGuard, deleteCustomer)
 
 export default customerRouter
